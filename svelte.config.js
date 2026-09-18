63115fce4dfa6c4823fbc37f9a4bdfbf8d4bbd32
HQ\901083 <Sonnya.Ghandi@bni.co.id>
2026-09-18 16:21:47 +0700

    fix: update @sveltejs/adapter-vercel to version 6.3.4 and improve svelte.config.js structure

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-export default { preprocess: vitePreprocess(), kit: { adapter: adapter() } };
+
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
